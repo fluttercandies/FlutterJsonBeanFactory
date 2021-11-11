72ad779e42bb1fdd330fc00ff840993df78a90ef
张瑞宇 <15201231801@163.com>
2021-11-11 15:49:51 +0800

    优化

--- src/main/kotlin/com/github/zhangruiyu/flutterjsonbeanfactory/action/migrate/OldDartClassNodeMigrate.kt
+++ src/main/kotlin/com/github/zhangruiyu/flutterjsonbeanfactory/action/migrate/OldDartClassNodeMigrate.kt
@@ -14,6 +14,7 @@ object OldDartClassNodeMigrate {
     fun getDartFileHelperClassGeneratorInfo(project: Project, pubSpecConfig: PubSpecConfig, file: PsiFile) {
         //不包含JsonConvert 那么就不转
         if ((file.text.contains("JsonConvert<")) && file.name != "json_convert_content.dart") {
+            val className: MutableList<String> = mutableListOf()
             file.children.forEach {
                 val text = it.text
                 val classNode = it?.node
@@ -23,14 +24,13 @@ object OldDartClassNodeMigrate {
                 )) && text.contains(
                     "JsonConvert<"
                 )
-                var className: String? = null
                 if (classNode?.elementType == DartTokenTypes.CLASS_DEFINITION && (isExtendsJsonConvert)
                 ) {
                     if (classNode is CompositeElement) {
                         for (filedAndMethodNode in classNode.children()) {
                             val nodeName = filedAndMethodNode.text
                             if (filedAndMethodNode.elementType == DartTokenTypes.COMPONENT_NAME) {
-                                className = nodeName
+                                className.add(nodeName)
                             } else if (filedAndMethodNode.elementType == DartTokenTypes.MIXINS) {
                                 //不包含JsonConvert 那么就不转
                                 if (nodeName.contains("JsonConvert").not()) {
@@ -41,51 +41,57 @@ object OldDartClassNodeMigrate {
                         }
                     }
                 }
-                if (className != null) {
-                    val replaceWith = if (file.text.contains("with JsonConvert<${className}> {")) {
-                        "with JsonConvert<${className}> {"
-                    } else if (file.text.contains("with JsonConvert<${className}>{")) {
-                        "with JsonConvert<${className}>{"
-                    } else if (file.text.contains("with JsonConvert<${className}>  {")) {
-                        "with JsonConvert<${className}>  {"
-                    } else if (file.text.contains("with JsonConvert<${className}>\n{")) {
-                        "with JsonConvert<${className}>\n{"
-                    } else if (file.text.contains("with JsonConvert<${className}>\t{")) {
-                        "with JsonConvert<${className}>\t{"
-                    } else if (file.text.contains("with JsonConvert<${className}>\n\t{")) {
-                        "with JsonConvert<${className}>\n\t{"
-                    } else if (file.text.contains("extends JsonConvert<${className}> {")) {
-                        "extends JsonConvert<${className}> {"
-                    } else if (file.text.contains("extends JsonConvert<${className}>{")) {
-                        "extends JsonConvert<${className}>{"
-                    } else if (file.text.contains("extends JsonConvert<${className}>  {")) {
-                        "extends JsonConvert<${className}>  {"
-                    } else if (file.text.contains("extends JsonConvert<${className}>\n{")) {
-                        "extends JsonConvert<${className}>\n{"
-                    } else if (file.text.contains("extends JsonConvert<${className}>\t{")) {
-                        "extends JsonConvert<${className}>\t{"
-                    } else if (file.text.contains("extends JsonConvert<${className}>\n\t{")) {
-                        "extends JsonConvert<${className}>\n\t{"
-                    } else {
-                        "JsonConvert<${className}> {"
-                    }
-
-                    val result = """
-import 'package:${pubSpecConfig.name}/generated/json/${File(file.name).nameWithoutExtension}_helper.dart';
-${
-                        file.text.lines().filterNot { itemLine ->
-                            itemLine.contains("json_convert_content.dart")
-                        }.joinToString("\n").replace("class $className", "@JsonSerializable()\nclass $className")
-                            .replace(
-                                replaceWith,
-                                "{\n\n\tfactory ${className}.fromJson(Map<String, dynamic> json) => $${className}FromJson(json);\n\n" +
-                                        "\tMap<String, dynamic> toJson() => $${className}ToJson(this);\n"
-                            )
-                    }
-                """.trimIndent()
-                    file.virtualFile.commitContent(project, result)
-                }
             }
+            className.forEach {itemClass->
+                val replaceWith = if (file.text.contains("with JsonConvert<${itemClass}> {")) {
+                    "with JsonConvert<${itemClass}> {"
+                } else if (file.text.contains("with JsonConvert<${itemClass}>{")) {
+                    "with JsonConvert<${itemClass}>{"
+                } else if (file.text.contains("with JsonConvert<${itemClass}>  {")) {
+                    "with JsonConvert<${itemClass}>  {"
+                } else if (file.text.contains("with JsonConvert<${itemClass}>\n{")) {
+                    "with JsonConvert<${itemClass}>\n{"
+                } else if (file.text.contains("with JsonConvert<${itemClass}>\t{")) {
+                    "with JsonConvert<${itemClass}>\t{"
+                } else if (file.text.contains("with JsonConvert<${itemClass}>\n\t{")) {
+                    "with JsonConvert<${itemClass}>\n\t{"
+                } else if (file.text.contains("extends JsonConvert<${itemClass}> {")) {
+                    "extends JsonConvert<${itemClass}> {"
+                } else if (file.text.contains("extends JsonConvert<${itemClass}>{")) {
+                    "extends JsonConvert<${itemClass}>{"
+                } else if (file.text.contains("extends JsonConvert<${itemClass}>  {")) {
+                    "extends JsonConvert<${itemClass}>  {"
+                } else if (file.text.contains("extends JsonConvert<${itemClass}>\n{")) {
+                    "extends JsonConvert<${itemClass}>\n{"
+                } else if (file.text.contains("extends JsonConvert<${itemClass}>\t{")) {
+                    "extends JsonConvert<${itemClass}>\t{"
+                } else if (file.text.contains("extends JsonConvert<${itemClass}>\n\t{")) {
+                    "extends JsonConvert<${itemClass}>\n\t{"
+                } else {
+                    "JsonConvert<${itemClass}> {"
+                }
+                var addImport = if (file.text.contains("json_field.dart").not()) {
+                    "import 'package:${pubSpecConfig.name}/generated/json/base/json_field.dart';\n"
+                } else ""
+                addImport += "import 'package:${pubSpecConfig.name}/generated/json/${File(file.name).nameWithoutExtension}_helper.dart';"
+                val result = """
+$addImport
+${
+                    file.text.lines().filterNot { itemLine ->
+                        itemLine.contains("json_convert_content.dart")
+                    }.joinToString("\n").replace("class $itemClass ", "@JsonSerializable()\nclass $itemClass ")
+                        .replace("class $itemClass\n", "@JsonSerializable()\nclass $itemClass ")
+                        .replace(
+                            replaceWith,
+                            "{\n\n${itemClass}();" +
+                            "\n\n\tfactory ${itemClass}.fromJson(Map<String, dynamic> json) => $${itemClass}FromJson(json);\n\n" +
+                                    "\tMap<String, dynamic> toJson() => $${itemClass}ToJson(this);\n"
+                        )
+                }
+                """.trimIndent()
+                file.virtualFile.commitContent(project, result)
+            }
+
         }
     }
 }
