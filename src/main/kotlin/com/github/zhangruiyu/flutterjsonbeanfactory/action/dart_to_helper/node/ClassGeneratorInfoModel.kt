c20ad8a91eec6cb51e276a9c97fc54ea019017ae
张瑞宇 <15201231801@163.com>
2021-11-26 11:01:56 +0800

    bugfix

--- src/main/kotlin/com/github/zhangruiyu/flutterjsonbeanfactory/action/dart_to_helper/node/ClassGeneratorInfoModel.kt
+++ src/main/kotlin/com/github/zhangruiyu/flutterjsonbeanfactory/action/dart_to_helper/node/ClassGeneratorInfoModel.kt
@@ -73,7 +73,7 @@ class HelperClassGeneratorInfo {
         if (isListType) {
             //如果泛型里带null
             if (getListSubTypeCanNull(type).endsWith("?")) {
-                stringBuilder.append("final ${getListSubType(type)}? $classFieldName = jsonConvert.convertList<${getListSubType(type)}>(json['${getJsonName}']);\n")
+                stringBuilder.append("final List<${getListSubType(type)}?>? $classFieldName = jsonConvert.convertList<${getListSubType(type)}>(json['${getJsonName}']);\n")
             } else {
                 stringBuilder.append("final List<${getListSubType(type)}>? $classFieldName = jsonConvert.convertListNotNull<${getListSubType(type)}>(json['${getJsonName}']);\n")
             }
